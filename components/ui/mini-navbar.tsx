fccf5b31747a91e01ecf35c3bb593cfe4d6a3936
Aaron dwm <aaron@diwaniyamedia.com>
2026-03-22 02:18:57 +0300

    describe what you changed

--- components/ui/mini-navbar.tsx
+++ components/ui/mini-navbar.tsx
@@ -213,7 +213,7 @@ export function Navbar() {
             src="/D*M website.png"
             alt="DWM"
             className="h-10 w-auto"
-            style={{ clipPath: "inset(12% 12% 12% 12%)" }}
+            style={{ clipPath: "inset(20% 20% 20% 20%)", transform: "scale(1.5)" }}
           />
         </a>
 
